fade5f6e9d89b5c535668520bfbe84cd40cc5b97
Victor Deon <victorhad@gmail.com>
2025-05-21 18:37:31 -0300

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Victor Deon on 21/05/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
